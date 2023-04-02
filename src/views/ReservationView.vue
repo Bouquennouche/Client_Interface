416a38d25d21962bd91a694e22e75a651794bccc
N.Chakir <nisrine_chakir@yahoo.com>
2023-04-02 16:50:16 +0000

    Update ReservationView.vue

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -1,75 +1,176 @@
 <template>
   <div class="container bg-light my-4 rounded w-75 shadow">
     <div class="row">
-      <div class="col-lg-5 imagbg">
-      </div>
+      <div class="col-lg-5 imagbg"></div>
       <div class="col py-5">
-        <form class="row g-3 px-5">
-            <h1>Reservation</h1>
+        <div class="row g-3 px-5">
+          <h1>Reservation</h1>
           <div class="col-md-6">
             <label for="inputEmail4" class="form-label">Nom</label>
-            <input type="text" class="form-control"  />
+            <input type="text" class="form-control" v-model="nom" />
           </div>
           <div class="col">
             <label for="inputPassword4" class="form-label">Prenom</label>
-            <input type="text" class="form-control"  />
+            <input type="text" class="form-control" v-model="prenom" />
           </div>
           <div class="col-12">
-            <label for="inputAddress" class="form-label">Numero de telephone</label>
-            <input
-              type="number"
-              class="form-control"
-            />
+            <label for="inputAddress" class="form-label"
+              >Numero de telephone</label
+            >
+            <input type="number" class="form-control" v-model="num_tel" />
           </div>
           <div class="col-6">
             <label class="form-label">Genre</label>
-            <select class="form-select" aria-label="Default select example">
-                <option selected value="Homme">Homme</option>
-                <option value="Femme">Femme</option>
+            <select
+              class="form-select"
+              aria-label="Default select example"
+              v-model="genre"
+            >
+              <option selected value="Homme">Homme</option>
+              <option value="Femme">Femme</option>
             </select>
           </div>
           <div class="col-6">
-            <label for="inputAddress" class="form-label">Date naissence</label>
-            <input type="date" class="form-control" />
+            <label for="inputAddress" class="form-label">Date naissance</label>
+            <input type="date" class="form-control" v-model="date" />
           </div>
           <div class="col-12">
             <label for="inputAddress" class="form-label">Service</label>
-            <input
-              type="number"
+            <select
               class="form-control"
+              v-model="serviceid"
+              placeholder="Selectionner une service"
+            >
+              <option v-for="item in services" :value="item.id" :key="item.id">
+                {{ item.nom }}
+              </option>
+            </select>
+          </div>
+          <div class="col-md-6">
+            <label for="inputAddress" class="form-label"
+              >Date de reservation</label
+            >
+            <input
+              type="date"
+              class="form-control"
+              v-model="date_rsv"
+              @input="getHoraireTravail()"
             />
           </div>
           <div class="col-md-6">
-            <label for="inputAddress" class="form-label">Date de reservation</label>
-            <input type="date" class="form-control"  />
-          </div>
-          <div class="col-md-6">
             <label class="form-label">Horaire</label>
-            <input type="text" class="form-control"  />
+            <select
+              class="form-select"
+              aria-label="Default select example"
+              v-model="horaire"
+              placeholder="Selectionner un horaire"
+            >
+              <option
+                v-for="item in horaireTravail"
+                v-bind:style="getItemStyle(item)"
+                v-bind:disabled="checkIfInArray(item)"
+                :value="item"
+                :key="horaireTravail.indexOf(item)"
+              >
+                {{ item }}
+              </option>
+            </select>
           </div>
-          
+
           <div class="col-12 mt-4">
-            <button class="btn btn-success">Reserver</button>
+            <button class="btn btn-success" @click="addrsv()">Reserver</button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  data:function(){
-    return{
-      horaireTravail:[],
-    }
-  }
+  data: function () {
+    return {
+      horaireTravail: [
+        "09:00:00",
+        "09:30:00",
+        "10:00:00",
+        "10:30:00",
+        "11:00:00",
+        "11:30:00",
+        "13:00:00",
+        "13:30:00",
+        "14:00:00",
+        "14:30:00",
+        "15:00:00",
+        "15:30:00",
+        "16:00:00",
+        "16:30:00",
+      ],
+      temp: [],
+      services: [],
+      horaire: "",
+      date_rsv: "",
+      nom: "",
+      prenom: "",
+      date: "",
+      serviceid: "",
+      genre: "",
+      horaire: "",
+      num_tel: "",
+    };
+  },
+  created() {
+    this.getSers();
+  },
+  methods: {
+    async getSers() {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/service");
+        this.services = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async addrsv() {
+      await axios.post(`http://127.0.0.1:8000/api/rendez_vous`, {
+        nom: this.nom,
+        prenom: this.prenom,
+        genre: this.genre,
+        date_naissance: this.date,
+        num_tel: this.num_tel,
+        date_rdv: this.date_rsv,
+        temp_dep: this.horaire,
+        id_ser: this.serviceid,
+      });
+    },
+    async getHoraireTravail() {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/heures`, {
+          params: {
+            date: this.date_rsv,
+          },
+        });
+        this.temp = response.data.heures;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    checkIfInArray(item) {
+      return this.temp.includes(item);
+    },
+    getItemStyle(item) {
+      return this.checkIfInArray(item)
+        ? { color: "white", background: "#de4452" }
+        : {};
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  .imagbg{
-      background-image: url('@/assets/imgs/calandar.jpg') !important;
-      background-size: cover !important;
-  }
+.imagbg {
+  background-image: url("@/assets/imgs/calandar.jpg") !important;
+  background-size: cover !important;
+}
 </style>
