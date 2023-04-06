697cb80128ecf06e8dfb8fd6210ba1823af1d679
N.Chakir <nisrine_chakir@yahoo.com>
2023-04-06 16:57:24 +0000

    Update LandingVue.vue

--- src/components/Home/LandingVue.vue
+++ src/components/Home/LandingVue.vue
@@ -9,26 +9,25 @@
             data-aos-delay="200"
           >
             <h1>Ameliorer l'experience de reservation</h1>
-            <h2>
-              Prenez un rendez-vous avec une facon facile, simple et rapide
-            </h2>
+            <h2>Prenez un rendez-vous d'une facon facile, simple et rapide</h2>
             <div class="d-flex justify-content-center justify-content-lg-start">
               <router-link to="/reservation">
-                <button class="btn btn-primary rounded-pill me-3 px-3 py-2" type="submit" to="/reservation">
-                <i class="bi bi-calendar4-week me-2"/>
-                Reserver maintenant
+                <button
+                  class="btn btn-primary rounded-pill me-3 px-3 py-2"
+                  type="submit"
+                  to="/reservation"
+                >
+                  <i class="bi bi-calendar4-week me-2" />
+                  Reserver maintenant
                 </button>
               </router-link>
-              
             </div>
           </div>
           <div
             class="col-lg-6 order-1 order-lg-2 hero-img"
             data-aos="zoom-in"
             data-aos-delay="200"
-          >
-
-          </div>
+          ></div>
         </div>
       </div>
     </section>
@@ -37,23 +36,24 @@
 
 <script>
 import "@/assets/sass/views/_landing.scss";
-import ContactVue from './Contact.vue';
+import ContactVue from "./Contact.vue";
 
 export default {
   name: "LandingVue",
-  components: {ContactVue}
+  components: { ContactVue },
 };
 </script>
 
 <style lang="scss" scoped>
-  h1,h2{
-    color: white !important;
-  }
-  #hero{
-    background-image: url('@/assets/imgs/bg.jpeg') !important;
-    background-repeat: no-repeat;
-    background-size: cover;
-    margin-top: -100px;
-    min-height:90vh !important;
-  }
+h1,
+h2 {
+  color: white !important;
+}
+#hero {
+  background-image: url("@/assets/imgs/bg.jpeg") !important;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-top: -100px;
+  min-height: 90vh !important;
+}
 </style>
